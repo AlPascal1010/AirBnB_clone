f8b8b52f60bdc12c630ff9df38d211e7bbf70f21
George Pascal <georgepascal1010@gmail.com>
2024-05-04 01:36:37 +0100

    README.md

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""Creates a unique FileStorage instance for the application"""
+
 from models.engine.file_storage import FileStorage
 
 
